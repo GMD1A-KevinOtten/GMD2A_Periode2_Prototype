bccfb82ea503d6c6586f8d54b3eaa8df176b513b
gmd1apeterbos <97040824@st.deltion.nl>
2017-11-21 11:43:34 +0100

    Placeholder shizzle

---
+++ TrainExportCompany/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+    public List<GameObject> wp = new List<GameObject>();
+    private bool inRange;
+    private int index;
+
+    public float speed;
+    public Transform target;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        MoveToWaypoint();
+	}
+
+    public void MoveToWaypoint()
+    {
+        if (inRange == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, wp[index].transform.position, speed * Time.deltaTime);
+            target = wp[index].transform;
+            Vector3 targetDir = target.position - transform.position;
+            Vector3 lookDir = Vector3.RotateTowards(transform.forward, targetDir, speed, 0.0F);
+            Quaternion rot = Quaternion.LookRotation(lookDir);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rot, speed * Time.deltaTime);
+            if (index < wp.Count)
+            {
+                if (transform.position == wp[index].transform.position)
+                {
+                    index++;
+                }
+            }
+            if (index == wp.Count)
+            {
+                index = 0;
+            }
+        }
+    }
+}
