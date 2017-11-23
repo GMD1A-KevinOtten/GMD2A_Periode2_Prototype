67915b44c704c4aded865a91aa844fffdaf2325f
Kevin Otten <kevin.otten@ziggo.nl>
2017-11-23 11:24:30 +0100

    Menu

---
+++ TrainExportCompany/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	
+	public static void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public static void SceneChance(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
---
+++ TrainExportCompany/Assets/Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public void Quit()
+    {
+        GameManager.QuitGame();
+    }
+
+    public void Start()
+    {
+        GameManager.SceneChance(1);
+    }
+}
