83a148fdd20800304514ed87270a114a9a2505a7
gmd1apeterbos <97040824@st.deltion.nl>
2017-11-26 16:27:13 +0100

    Some new scripts that don't do a lot

---
+++ TrainExportCompany/Assets/Scripts/Train.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train : MonoBehaviour {
+    public float speed;
+    public float cartOffset;
+
+    public List<GameObject> trainCarts = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+        GetComponent<WaypointsNew>().speed = speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(GetComponent<WaypointsNew>().speed != speed)
+        {
+            GetComponent<WaypointsNew>().speed = speed;
+        }
+    }
+}
---
+++ TrainExportCompany/Assets/Scripts/Traincart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traincart : MonoBehaviour {
+    
+
+    //WERKT NOG NIET
+
+
+    //private GameObject train;
+    //private GameObject cartInFront;
+    //private Train trainScript;
+	// Use this for initialization
+	void Start () {
+        //train = GameObject.FindGameObjectWithTag("Train");
+        //trainScript = train.GetComponent<Train>();
+
+        //cartInFront = trainScript.trainCarts[trainScript.trainCarts.IndexOf(gameObject) -1];
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Move();
+	}
+
+    void Move()
+    {
+        //Vector3 v = new Vector3(cartInFront.transform.position.x - trainScript.cartOffset, cartInFront.transform.position.y, cartInFront.transform.position.z);
+
+        //transform.position = Vector3.MoveTowards(transform.position, v, trainScript.speed * Time.deltaTime);
+
+        //Vector3 targetDir = cartInFront.transform.position - transform.position;
+        //Vector3 lookDir = Vector3.RotateTowards(transform.forward, targetDir, trainScript.speed, 0.0F);
+        //Quaternion rot = Quaternion.LookRotation(lookDir);
+        //transform.rotation = Quaternion.Lerp(transform.rotation, rot, trainScript.speed * Time.deltaTime);
+    }
+}
