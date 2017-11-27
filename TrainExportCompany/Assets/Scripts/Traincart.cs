a362537897a68db6626bc9f27f39e55aec3f84ab
gmd1apeterbos <97040824@st.deltion.nl>
2017-11-27 22:40:11 +0100

    Wagon script

--- TrainExportCompany/Assets/Scripts/Traincart.cs
+++ TrainExportCompany/Assets/Scripts/Traincart.cs
@@ -3,36 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Traincart : MonoBehaviour {
-    
-
-    //WERKT NOG NIET
-
-
-    //private GameObject train;
-    //private GameObject cartInFront;
-    //private Train trainScript;
-	// Use this for initialization
-	void Start () {
-        //train = GameObject.FindGameObjectWithTag("Train");
-        //trainScript = train.GetComponent<Train>();
-
-        //cartInFront = trainScript.trainCarts[trainScript.trainCarts.IndexOf(gameObject) -1];
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        Move();
-	}
-
-    void Move()
+    public List<GameObject> wp = new List<GameObject>();
+    private GameObject nextWp;
+    private Train train;
+    void Start()
     {
-        //Vector3 v = new Vector3(cartInFront.transform.position.x - trainScript.cartOffset, cartInFront.transform.position.y, cartInFront.transform.position.z);
+        train = GameObject.FindGameObjectWithTag("Train").GetComponent<Train>();
+        nextWp = wp[0];
+        
+    }
 
-        //transform.position = Vector3.MoveTowards(transform.position, v, trainScript.speed * Time.deltaTime);
+    void Update()
+    {
+        if(nextWp == null)
+        {
 
-        //Vector3 targetDir = cartInFront.transform.position - transform.position;
-        //Vector3 lookDir = Vector3.RotateTowards(transform.forward, targetDir, trainScript.speed, 0.0F);
-        //Quaternion rot = Quaternion.LookRotation(lookDir);
-        //transform.rotation = Quaternion.Lerp(transform.rotation, rot, trainScript.speed * Time.deltaTime);
+           
+            foreach(Transform child in train.trainCarts[train.trainCarts.IndexOf(gameObject) - 1].transform)
+            {
+                if (child.tag == "FollowCart")
+                {
+                    wp[0] = child.gameObject;
+                }
+            }
+            nextWp = wp[0];
+        }
+        if (nextWp != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, nextWp.transform.position, train.speed  * Time.deltaTime);
+
+            Vector3 targetDir = nextWp.transform.position - transform.position;
+            Vector3 lookDir = Vector3.RotateTowards(transform.forward, targetDir, train.speed * 6000, 0.0F);
+            Quaternion rot = Quaternion.LookRotation(lookDir);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rot, train.speed * 6000 * Time.deltaTime);
+
+        }
     }
 }
